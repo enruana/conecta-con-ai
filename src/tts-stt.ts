40acdbce4569025202e1bcf8681688a16cdd8beb
Felipe Mantilla <andres.mantilla@gorillalogic.com>
2025-01-24 14:49:02 -0500

    feat: speech to text implementation

--- src/tts-stt.ts
+++ src/tts-stt.ts
@@ -8,11 +8,28 @@ export async function testTextToSpeech(): Promise<void> {
     model: 'tts-1',
     voice: 'echo',
     input:
-      '¡Hola! Me llamo Felipe y estoy aquí para ayudarte a aprender sobre el mundo de la IA!',
+      'Hallo! Ich heiße Felipe und ich bin hier, um dir zu helfen, die Welt der KI kennenzulernen!',
   });
 
   const buffer = Buffer.from(await mp3.arrayBuffer());
-  await fs.promises.writeFile('text-to-speach.mp3', buffer);
+  await fs.promises.writeFile('text-to-speech.mp3', buffer);
 }
 
-export async function testSpeechToText(): Promise<void> {}
+export async function testSpeechToText(): Promise<void> {
+
+  const openai = new OpenAI();
+
+  const transcription = await openai.audio.transcriptions.create({
+    file: fs.createReadStream('text-to-speech.mp3'),
+    model: 'whisper-1',
+  });
+
+  const translation = await openai.audio.translations.create({
+    file: fs.createReadStream('text-to-speech.mp3'),
+    model: 'whisper-1',
+    prompt: 'translate to english',
+  });
+
+  console.log('Transcription: ', transcription.text);
+  console.log('Translation: ', translation.text);
+}
